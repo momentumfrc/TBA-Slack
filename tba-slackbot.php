89f24f3b86433c10453b90b89a65ca19ec24b889
Jordan Powers <jordanpowers1227@gmail.com>
2017-06-13 16:19:31 -0700

    Squashed a Bug

--- tba-slackbot.php
+++ tba-slackbot.php
@@ -12,7 +12,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       $data_string = '{"text": "Match coming up!","attachments":[';
       foreach($md["team_keys"] as $key) {
         $team = str_replace("frc","",$key);
-        $data_string = $data_string . '{ "text": "<https://momentum4999.com/scouting/info.php?team="'.$team.'|Team '.$team.'>" },';
+        $data_string = $data_string . '{ "text": "<https://momentum4999.com/scouting/info.php?team='.$team.'|Team '.$team.'>" },';
       }
       $data_string = rtrim($data_string,",");
       $data_string = $data_string . ']}';

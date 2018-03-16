18fa68741ebe5c1ab44d6dee6e3e8588385628c4
Jordan Powers <jordanpowers1227@gmail.com>
2018-03-16 14:28:02 -0700

    Added 7042

--- tba-slackbot.php
+++ tba-slackbot.php
@@ -19,7 +19,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       foreach($match["alliances"]["blue"]["team_keys"] as $key) {
         $team = str_replace("frc","",$key);
         $data_string = $data_string . '{ "text": "<https://momentum4999.com/scouting/info.php?team='.$team.'|Team '.$team.'>"';
-        if($team == "4999") {
+        if($team == "4999" || $team == "7042") {
           $data_string = $data_string . ', "color" : "#06ceff"';
         } else {
           $data_string = $data_string . ', "color" : "#148be5"';
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       foreach($match["alliances"]["red"]["team_keys"] as $key) {
         $team = str_replace("frc","",$key);
         $data_string = $data_string . '{ "text": "<https://momentum4999.com/scouting/info.php?team='.$team.'|Team '.$team.'>"';
-        if($team == "4999") {
+        if($team == "4999" || $team == "7042") {
           $data_string = $data_string . ', "color" : "#ff2200"';
         } else {
           $data_string = $data_string . ', "color" : "#d60c0c"';
@@ -58,6 +58,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 					$message = '{"text": "Better luck next time!\n Match '.$match["match_number"].' lost '.$match["alliances"]["red"]["score"].'-*'.$match["alliances"]["blue"]["score"].'*"}';
 				} elseif($match["alliances"]["red"]["score"] == $match["alliances"]["blue"]["score"]) {
 					$message = '{"text": "Good job Momentum!\n Match '.$match["match_number"].' tied '.$match["alliances"]["red"]["score"].'-'.$match["alliances"]["blue"]["score"].'"}';
+				}
+			} elseif(in_array("frc7042",$match["alliances"]["blue"]["teams"])) {
+				if($match["alliances"]["blue"]["score"] > $match["alliances"]["red"]["score"]) {
+					$message = '{"text": "Congratulations Rabbotics!\n Match '.$match["match_number"].' won *'.$match["alliances"]["blue"]["score"].'*-'.$match["alliances"]["red"]["score"].'"}';
+				} elseif($match["alliances"]["blue"]["score"] < $match["alliances"]["red"]["score"]) {
+					$message = '{"text": "Better luck next time!\n Match '.$match["match_number"].' lost '.$match["alliances"]["blue"]["score"].'-*'.$match["alliances"]["red"]["score"].'*"}';
+				} elseif($match["alliances"]["blue"]["score"] == $match["alliances"]["red"]["score"]) {
+					$message = '{"text": "Good job Rabbotics!\n Match '.$match["match_number"].' tied '.$match["alliances"]["blue"]["score"].'-'.$match["alliances"]["red"]["score"].'"}';
+				}
+			} elseif(in_array("frc7042",$match["alliances"]["red"]["teams"])) {
+				if($match["alliances"]["red"]["score"] > $match["alliances"]["blue"]["score"]) {
+					$message = '{"text": "Congratulations Rabbotics!\n Match '.$match["match_number"].' won *'.$match["alliances"]["red"]["score"].'*-'.$match["alliances"]["blue"]["score"].'"}';
+				} elseif($match["alliances"]["red"]["score"] < $match["alliances"]["blue"]["score"]) {
+					$message = '{"text": "Better luck next time!\n Match '.$match["match_number"].' lost '.$match["alliances"]["red"]["score"].'-*'.$match["alliances"]["blue"]["score"].'*"}';
+				} elseif($match["alliances"]["red"]["score"] == $match["alliances"]["blue"]["score"]) {
+					$message = '{"text": "Good job Rabbotics!\n Match '.$match["match_number"].' tied '.$match["alliances"]["red"]["score"].'-'.$match["alliances"]["blue"]["score"].'"}';
 				}
 			} else {
 				$message = $message = '{"text": "Match Complete!\n Match '.$match["match_number"].' finished '.$match["alliances"]["blue"]["score"].'-'.$match["alliances"]["red"]["score"].'"}';
